1d2a4e2f6b3b34559fb728b8562b2191d2348503
chenghuang2016 <237365815@qq.com>
2016-03-13 22:07:38 +0800

    Added files via upload

---
+++ lev3.py
@@ -0,0 +1,17 @@
+
+     #level3
+
+import os
+car=["     ###############","    #               ##","####                  ##","#                       ##########  ","### # ####################   #   ##","  #   #                    #   #    ","    #                        #     "]  #to disgn a symble of a car
+def show():
+    for l in range(30):
+        for i in range(7):
+            for j in range(l):
+                print " ",
+            print car[i]
+        x=0x2fffff
+        while(x):
+            pass
+            x=x-1
+        os.system("clear")
+show()
